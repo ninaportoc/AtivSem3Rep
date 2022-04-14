c3208bbe5818c8ab7a94172c78a1b33a623cab76
Marina Porto <100438303+ninaportoc@users.noreply.github.com>
2022-04-14 14:55:40 -0300

    Update 7divisao.js

--- 7divisao.js
+++ 7divisao.js
@@ -1,12 +1,11 @@
 function divisaoEntreParametros(n1,n2){
-if (n1/n2 && n1 % n2 === 0 ){
-    return 'O resultado da divisão é' + ' ' + (n1/n2) + '  ' + ' e o resto desta divisão é' + '  ' + (n1 % n2) + '  ' + 'e o número é par'
-} else if (n2/n1 && n2 % n1 === 0) {
-    return 'O resultado da divisão é' + ' ' + (n2/n1) + '  ' + ' e o resto desta divisão é' + '  ' + (n2 % n1) + '  ' + 'e o número é par'
-} else if (n2/n1 && n2 % n1 > 0 ||  n2 % n1 < 0) {
-return 'O resultado desta divisão é' + ' ' + (n2/n1) 
-} else if (n1/n2 && n1 % n2 > 0 ||  n1 % n2 < 0) {
-    return 'O resultado desta divisão é' + ' ' + (n1/n2) 
+if (n1/n2 && n1/n2 % 2 === 0 ){
+    return 'O resultado da divisão é' + ' ' + (n1/n2) + ' ' + 'e este número é par'
+} else if (n2/n1 && n2/n1 % 2 === 0) {
+    return 'O resultado da divisão é' + ' ' + (n2/n1) + '  ' + 'e este número é par'
+} else if (n2/n1 && n2/n1 % 2 === 1) {
+return 'O resultado desta divisão é' + ' ' + (n2/n1) + '  ' + 'e este número é ímpar'
+} else if (n1/n2 && n1/n2 % 2 === 1) {
+    return 'O resultado desta divisão é' + ' ' + (n1/n2) + '  ' + 'e este número é ímpar'
 }
 }
-
